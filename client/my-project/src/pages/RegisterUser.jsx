9e78fae7388241803d31c693d0fcc3570e596de6
Shivam Kumar <satishshivam697@gmail.com>
2023-12-27 17:56:03 +0530

    Updated

--- client/my-project/src/pages/RegisterUser.jsx
+++ client/my-project/src/pages/RegisterUser.jsx
@@ -38,7 +38,7 @@ const RegisterUser = () => {
         break;
         case 'email':
           // Updated email validation regular expression
-          if (/^\S+@\S+\.\S+$/.test(value) || value === '') {
+          if (/^[a-zA-Z0-9._]+@[a-z]+\.[a-z]{2-6}$/.test(value) || value === '') {
             setFormData({
               ...formData,
               [name]: value,
